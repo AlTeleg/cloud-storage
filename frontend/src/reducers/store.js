90c716feafa1a121581cd6e0449a60ad1866931c
Александр <alteleg@yandex.ru>
2024-03-18 20:16:50 +0300

    Fixes

--- frontend/src/reducers/store.js
+++ frontend/src/reducers/store.js
@@ -6,8 +6,10 @@ import authReducer from './auth.js';
 const persistedReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
-  reducer: persistedReducer
-});
+  reducer: {
+    auth: persistedReducer
+  }
+})
 
 export const persistor = persistStore(store);
 export default store;
