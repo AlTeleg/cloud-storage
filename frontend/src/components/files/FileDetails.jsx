c3493e6f442946fec758bc8348d6b83a6f3c56ac
Александр <alteleg@yandex.ru>
2024-03-28 02:24:16 +0300

    Fixes

--- frontend/src/components/files/FileDetails.jsx
+++ frontend/src/components/files/FileDetails.jsx
@@ -36,6 +36,18 @@ const FileDetails = () => {
     }
   };
 
+  const base64ToArrayBuffer = async (base64) => {
+    const binaryString = window.atob(base64);
+    const length = binaryString.length;
+    const bytes = new Uint8Array(length);
+  
+    for (let i = 0; i < length; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+  
+    return bytes.buffer;
+  }
+
   if (!file) {
     return <div>Loading file details...</div>;
   }
@@ -48,12 +60,12 @@ const FileDetails = () => {
     <>
       {fileExtension === 'txt' ? (
         <>
-          <p>{new FileReader().readAsText(file.data)}</p>
+          <p>{new FileReader().readAsText(base64ToArrayBuffer(file.data))}</p>
           <br />
         </>
       ) : (
         mediaTypes.includes(fileExtension) ? (
-          <FileViewer fileType={fileExtension} filePath={file.data} />
+          <FileViewer fileType={fileExtension} filePath={base64ToArrayBuffer(file.data)} />
         ) : (
           <img src={fileImg} alt="File" />
         )
